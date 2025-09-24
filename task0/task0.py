46c4792b8814403caf4eee59114829dad294c584
bruhbzaaal <113934002+bruhbzaaal@users.noreply.github.com>
2025-09-25 00:09:31 +0300

    Update task0.py

--- task0/task0.py
+++ task0/task0.py
@@ -1,1 +1,33 @@
+import pandas as pd
 
+graph = pd.read_csv("task2.csv", header=None)
+
+edges = []
+
+for index, row in graph.iterrows():
+    a = int(row[0])
+    b = int(row[1])
+    edges.append([a, b])
+
+print("Ребра:", edges)
+
+number_of_vertexes = 0
+for u, v in edges:
+        if u > number_of_vertexes:
+            number_of_vertexes = u
+        if v > number_of_vertexes:
+            number_of_vertexes = v
+
+size = number_of_vertexes + 1
+matrix = []
+
+for i in range(size):
+     row = [0] * size
+     matrix.append(row)
+
+for i, j in edges:
+     matrix[i][j] = 1
+     matrix[j][i] = 1 
+
+for row in matrix:
+     print(row)
